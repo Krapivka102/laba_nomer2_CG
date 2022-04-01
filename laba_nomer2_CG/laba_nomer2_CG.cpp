6a289375eaef38e53f95685b6dffa873b8ef8a2e
Krapivka <niko_wellick@mail.ru>
2022-04-01 18:51:12 +0500

    Сделал 8 этап

--- laba_nomer2_CG/laba_nomer2_CG.cpp
+++ laba_nomer2_CG/laba_nomer2_CG.cpp
@@ -19,10 +19,11 @@ void RenderSceneCB()
     Scale += 0.001f;
 
     glm::mat4x4 World;
-    World[0][0] = cosf(Scale); World[0][1] = -sinf(Scale); World[0][2] = 0.0f; World[0][3] = 0.0f;
-    World[1][0] = sinf(Scale); World[1][1] = cosf(Scale);  World[1][2] = 0.0f; World[1][3] = 0.0f;
-    World[2][0] = 0.0f;        World[2][1] = 0.0f;         World[2][2] = 1.0f; World[2][3] = 0.0f;
-    World[3][0] = 0.0f;        World[3][1] = 0.0f;         World[3][2] = 0.0f; World[3][3] = 1.0f;
+    World[0][0] = sinf(Scale); World[0][1] = 0.0f;        World[0][2] = 0.0f;        World[0][3] = 0.0f;
+    World[1][0] = 0.0f;        World[1][1] = cosf(Scale); World[1][2] = 0.0f;        World[1][3] = 0.0f;
+    World[2][0] = 0.0f;        World[2][1] = 0.0f;        World[2][2] = sinf(Scale); World[2][3] = 0.0f;
+    World[3][0] = 0.0f;        World[3][1] = 0.0f;        World[3][2] = 0.0f;        World[3][3] = 1.0f;
+
     Vertices[0] = World * glm::vec4(Vertices[0], 1.0f);
     Vertices[1] = World * glm::vec4(Vertices[1], 1.0f);
     Vertices[2] = World * glm::vec4(Vertices[2], 1.0f);
